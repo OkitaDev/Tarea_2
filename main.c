2307b5416dea05333382cbaccd19f0adc4f423a8
OkitaDev <soujiok@gmail.com>
2021-04-16 20:36:52 -0400

    Se agrego el switch

--- main.c
+++ main.c
@@ -9,6 +9,34 @@ int main()
         mostrarMenuPokedex();
         scanf("%hi", &opcion);
         if(opcion == 0) break;
+        
+        switch(opcion)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 10:
+                break;
+            default:
+                printf("\nLa opcion que ha marcado no existe\n");
+                break;
+        }
+        printf("\n");
     } while (opcion != 0);
     
     printf("\n -----\n");
