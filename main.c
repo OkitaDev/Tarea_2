41c67a70af07e492026d61b0d0f60fc61bd1d314
OkitaDev <soujiok@gmail.com>
2021-04-16 20:28:59 -0400

    Intento de inicio de Menu

--- main.c
+++ main.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
+#include "Librerias/Interfaz/interfaz.h"
 
 int main()
 {
-    printf("Hola Mundo\n");
+    short opcion;
+    do
+    {
+        mostrarMenuPokedex();
+        scanf("%hi", &opcion);
+        if(opcion == 0) break;
+    } while (opcion != 0);
+    
+    printf("\n -----\n");
+    printf("| FIN |\n");
+    printf(" -----\n\n");
+
     return 0;
 }
