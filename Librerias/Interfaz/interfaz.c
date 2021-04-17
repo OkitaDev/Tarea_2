44988ce778c9a0cfcc43653973ceea3caaa9c334
OkitaDev <soujiok@gmail.com>
2021-04-16 20:42:06 -0400

    Intento de subir archivos

---
+++ Librerias/Interfaz/interfaz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void mostrarMenuPokedex()
+{
+    printf(" ---------\n");
+    printf("| POKEDEX |\n");
+    printf(" ---------\n\n");
+
+    printf("1.Importar/Exportar desde archivo .csv\n");
+    printf("2.Registrar Pokemon Atrapado\n");
+    printf("3.Evolucionar Pokemon\n");
+    printf("4.Buscar Pokemon por Tipo\n");
+    printf("5.Buscar mis Pokemon por Nombre\n");
+    printf("6.Buscar Pokemon en Pokedex por Nombre\n");
+    printf("7.Mostrar todos los Pokemons en Pokedex\n");
+    printf("8.Mostrar mis Pokemon ordenados por PC\n");
+    printf("9.Liberar un Pokemon\n");
+    printf("10.Mostrar Pokemon por Region\n");
+    printf("0.Salir de la Pokedex\n");
+    printf("\n");
+}
