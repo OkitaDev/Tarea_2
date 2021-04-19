9cfea0595304cf35a459aedc95d6b7d484dbd7c1
OkitaDev <soujiok@gmail.com>
2021-04-18 21:39:41 -0400

    Avances en Importar Archivo

--- Librerias/pokemon.c
+++ Librerias/pokemon.c
@@ -32,12 +32,23 @@ typedef struct tipoPokemon
     tipoIdentificacion ident;
 }tipoPokemon;
 
+void ingresarPokemon(HashMap * mapa, char * lineaLeida)
+{
+    tipoPokemon * nuevoPokemon = malloc (sizeof(tipoPokemon *));
+    char * fragmento;
+
+    fragmento = strtok(lineaLeida, ",");
+    printf("%s\n", fragmento);
+}
+
 HashMap * importarArchivo(HashMap * mapa)
 {
+    //Ingreso del nombre del archivo a implementar
     char nombreArchivo[40];
     printf("\nIngrese el nombre del archivo (Incluyendo el .cvs)\n\n");
     scanf("%39s", nombreArchivo);
 
+    //Busqueda del archivo
     FILE * archivo = fopen(nombreArchivo, "r");
 
     if(archivo == NULL)
@@ -46,7 +57,21 @@ HashMap * importarArchivo(HashMap * mapa)
         return NULL;
     }
 
+    //Lectura del archivo (saltandose la primera linea)
+    char lineaLeida[100];
+    short lecturaPrimeraLinea = 0;
+
+    while(fgets(lineaLeida, 99, archivo))
+    {
+        if(lecturaPrimeraLinea != 0)
+        {
+            ingresarPokemon(mapa, lineaLeida);
+        }
+        lecturaPrimeraLinea++;
+    }
+    
     printf("\nArchivo IMPORTO!\n");
+    fclose(archivo);
     return mapa;
 }
 
@@ -65,5 +90,6 @@ HashMap * exportarArchivo(HashMap * mapa)
     }
 
     printf("\nArchivo EXPORTADO!\n");
+    fclose(archivo);
     return mapa;
 }
--- Librerias/pokemon.h
+++ Librerias/pokemon.h
@@ -1,7 +1,12 @@
 #ifndef POKEMON_H
 #define POKEMON_H
 
+typedef struct tipoIdentificacion tipoIdentificacion;
+typedef struct tipoEvoluciones tipoEvoluciones;
+typedef struct tipoPuntos tipoPuntos;
 typedef struct tipoPokemon tipoPokemon;
+
+void ingresarPokemon(HashMap *, char *);
 HashMap * importarArchivo(HashMap *);
 HashMap * exportarArchivo(HashMap *);
 
